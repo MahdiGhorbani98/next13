a8086ae1c7d262a1ba63ce86d10b550f9cde9b59
oliver <mahdig502@gmail.com>
2023-07-22 04:01:33 +0330

    update in use server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,14 @@ import Link from "next/link";
 //   return response.json();
 // }
 
-async function getTodos() {
+function getTodos() {
   return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
 export default async function Home() {
@@ -31,7 +37,7 @@ export default async function Home() {
       </header>
       <ul>
         {todos?.map((todo: any) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,25 @@
+"use client";
 type TodoItemProps = {
-  id: number;
+  id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, compelete: boolean) => void;
 };
 
-export default function TodoItem({ id, title, complete }: TodoItemProps) {
+export default function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+}: TodoItemProps) {
   return (
     <li className="flex gap-1 items-center">
       <input
         type="checkbox"
         id={id.toString()}
+        defaultChecked={complete}
         className="cursor-pointer peer"
+        onChange={(e) => toggleTodo(id, e.target.checked)}
       />
       <label
         htmlFor={id.toString()}
