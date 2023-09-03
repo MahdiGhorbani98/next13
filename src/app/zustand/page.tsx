4b350924e98ae8e6a6d515533c6e9c4a4794a519
oliver <mahdig502@gmail.com>
2023-09-03 15:53:10 +0330

    hooks: useGetFromStore to clean code for persist

--- src/app/zustand/page.tsx
+++ src/app/zustand/page.tsx
@@ -1,25 +1,25 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useBearStore } from '@/store/BearStore';
+import { useGetFromStore } from '@/hooks/useGetFromStore';
 
 const Zustand = () => {
-  const { bears, increasePopulation, removeAllBears } = useBearStore(
-    (state) => ({
-      bears: state.bears,
-      increasePopulation: state.increasePopulation,
-      removeAllBears: state.removeAllBears,
-    }),
-  );
-  const [bearsCount, setBearsCount] = useState<number>();
-  useEffect(() => {
-    setBearsCount(bears);
-  }, [bears]);
+  const { increasePopulation, removeAllBears } = useBearStore((state) => ({
+    increasePopulation: state.increasePopulation,
+    removeAllBears: state.removeAllBears,
+  }));
+
+  //   const [bearsCount, setBearsCount] = useState<number>();
+  //   useEffect(() => {
+  //     setBearsCount(bears);
+  //   }, [bears]);
+  const bears = useGetFromStore(useBearStore, (state) => state.bears);
 
   return (
     <div>
       <section>
         <h1>Hi Zustand</h1>
-        <p>bears: {bearsCount}</p>
+        <p>bears: {bears}</p>
         <button
           className="bg-white text-black px-4"
           onClick={increasePopulation}
